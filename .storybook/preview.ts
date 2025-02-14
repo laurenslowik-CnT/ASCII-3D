4151544628c2a346b870efb50b113b73729d2280
mihirsuchakct <145479313+mihirsuchakct@users.noreply.github.com>
2025-02-14 18:49:38 +0530

    Chore: Enable app directory support in storybook (#78)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,14 @@ import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
+    nextjs: {
+      /**
+       * Tells Storybook to work with the `app` directory in Next.js.
+       * For more details
+       * @see https://storybook.js.org/docs/get-started/frameworks/nextjs#nextjs-navigation
+       */
+      appDirectory: true, 
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
